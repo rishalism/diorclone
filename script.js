ab3b9e1fb52b813d56cf27a94e3ca77ae150411c
rishalism <rishalmuhammedup@gmail.com>
2023-09-08 09:16:55 +0530

    added

---
+++ script.js
@@ -0,0 +1,20 @@
+function fashionhover(){
+const collapse =document.getElementById('collapseExample');
+collapse.style.display="block";
+}
+function fashionout(){
+    const collapse=document.getElementById('collapseExample');
+    collapse.style.display="none";
+}
+
+function  fragrancehover(){
+    const collapse=document.getElementById('collapseExample1');
+    collapse.style.display="block"
+}
+
+function  fragranceout(){
+    const collapse=document.getElementById('collapseExample1');
+    collapse.style.display="none"
+}
+
+
